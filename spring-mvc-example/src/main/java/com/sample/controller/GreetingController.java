269f88718bc36188af601937baa20139ff6ea1de
esmameral <esma.meral@gmail.com>
2021-10-24 15:50:38 +0300

    sample list was added

--- spring-mvc-example/src/main/java/com/sample/controller/GreetingController.java
+++ spring-mvc-example/src/main/java/com/sample/controller/GreetingController.java
@@ -1,5 +1,9 @@
 package com.sample.controller;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +22,16 @@ public class GreetingController {
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, 
 			@RequestParam(name="city", required=true) String city,Model model) {
 		Greeting greeting=service.prepareGreeting(name,city);
+		List<String> townList=new ArrayList<String>();
+		townList.add("Kadikoy");
+		townList.add("uskudar");
+		townList.add("Kartal");
+		
 		model.addAttribute("greeting", greeting);
+		model.addAttribute("townList", townList);
+		
+		
 		return "result";
+		
 	}
 }
